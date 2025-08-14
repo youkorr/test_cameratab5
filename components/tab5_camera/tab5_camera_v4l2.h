4da4144232c5172b4b6808196a74b77a0eccef17
youkorr <96953496+youkorr@users.noreply.github.com>
2025-08-14 02:54:10 +0200

    Update tab5_camera_v4l2.h

--- components/tab5_camera/tab5_camera_v4l2.h
+++ components/tab5_camera/tab5_camera_v4l2.h
@@ -26,7 +26,7 @@
 // Includes système nécessaires pour V4L2
 #include <fcntl.h>
 #include <sys/ioctl.h>
-#include <sys/mman.h>
+#include "components/tab5_camera/sys/mman.h"
 #include <sys/param.h>
 
 #include "freertos/FreeRTOS.h"
